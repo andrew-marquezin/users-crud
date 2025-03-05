5a29e29681e8c7422ed7eece4e18a4ef2d350fe5
Andrew Marquezin Marinho <andrewmmarinho@gmail.com>
2025-03-05 14:41:58 -0300

    ampliação do modelo e validação de entrada do usuário

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,10 +1,35 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IAddress extends Document {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  address: IAddress;
+  dateOfBirth: Date;
+  email: string;
+  documentNumber: string;
+}
+
+const addressSchema = new Schema({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+})
 
 const userScema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  address: addressSchema,
+  dateOfBirth: { type: Date, required: true },
   email: { type: String, required: true },
   documentNumber: { type: String, required: true },
 })
 
-export const UserModel = mongoose.model('User', userScema);
+export const UserModel = mongoose.model<IUser>('User', userScema);
