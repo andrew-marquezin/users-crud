2947cdf5d550eba3888409ba366bc73f2e920457
Andrew Marquezin Marinho <andrewmmarinho@gmail.com>
2025-03-20 13:25:23 -0300

    tipos do frontend

---
+++ frontend/src/types/UserType.ts
@@ -0,0 +1,45 @@
+export type AddressType = {
+  id: string;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+export type AddressInputDTO = {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+export type PhoneType = {
+  id: string;
+  number: string;
+  type: string;
+}
+
+export type PhoneInputDTO = {
+  number: string;
+  type: string;
+}
+
+export type UserType = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  addresses: AddressType[];
+  phoneNumbers: PhoneType[];
+  dateOfBirth: Date;
+  email: string;
+  documentNumber: string;
+}
+
+export type UserInputDTO = {
+  firstName: string;
+  lastName: string;
+  addresses: AddressInputDTO[];
+  phoneNumbers: PhoneInputDTO[];
+  email: string;
+  documentNumber: string;
+}
