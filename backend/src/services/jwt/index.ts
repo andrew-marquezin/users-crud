b046c70b1c31c3110c66640ba358cb3a142dae2a
Andrew Marquezin Marinho <andrewmmarinho@gmail.com>
2025-07-19 12:34:02 -0300

    pequenos ajustes e authController

--- backend/src/services/jwt/index.ts
+++ backend/src/services/jwt/index.ts
@@ -1,1 +1,24 @@
 import jwt from 'jsonwebtoken';
+import { config } from '../../config/config';
+
+
+export class JWTService {
+  static generateToken(apiKey: string): string {
+    const data = {
+      apiKey,
+    };
+
+    return jwt.sign(data, config.JWT_SECRET, {
+      expiresIn: config.JWT_EXPIRATION
+    });
+  }
+
+  static validateToken(token: string) {
+    try {
+      const decoded = jwt.verify(token, config.JWT_SECRET);
+      return decoded;
+    } catch (e) {
+      throw new Error('Invalid API key');
+    }
+  }
+}
